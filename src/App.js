14d49db369c3559ea3ee96308dbab60f5dd62c94
RobertoD <mitchelldurandb25@gmail.com>
2022-03-11 23:09:58 -0500

    agergando signin y footer al proyecto

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import './App.css';
 import Carrusel from './Components/Carrusel/Carrusel';
+import Footer2 from './Components/Footer/Footer2';
+
 import Navbar from './Components/Navbar/Navbar';
+import { SignIn } from './Components/Sign_in/SignIn';
+
 
 function App() {
   return (
     <>
       <Navbar />
+      <SignIn />
       <Carrusel />
       <Carrusel />
       <Carrusel />
+      <Footer2 />
     </>
   ); 
 }
