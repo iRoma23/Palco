001ff6f82184d74bb5f7dde2c985587d4018ef46
Romario <romario.jara.ch@gmail.com>
2022-03-11 23:19:32 -0500

    carrusel - autoplay

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import Carrusel from './Components/Carrusel/Carrusel';
 import Navbar from './Components/Navbar/Navbar';
 
 function App() {
+  const images = ['01.jpg', '02.jpg', '03.jpg'];
   return (
     <>
       <Navbar />
-      <Carrusel />
-      <Carrusel />
-      <Carrusel />
+      <Carrusel images={images} autoplay={true} />
+      <Carrusel images={images} />
+      <Carrusel images={images} />
     </>
   ); 
 }
