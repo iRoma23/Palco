7c66e0493e039dbf5b9ec9e6ff06f31d049fa92f
Romario <romario.jara.ch@gmail.com>
2022-03-16 13:07:38 -0500

    Componente informacion

---
+++ src/AppPrueba.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Informacion from './Components/Informacion/Informacion';
+import Navbar from './Components/Navbar/Navbar'
+
+function AppPrueba() {
+
+  return (
+    <>
+      <Navbar />
+      <Informacion />
+    </>
+  ); 
+}
+
+export default AppPrueba;
