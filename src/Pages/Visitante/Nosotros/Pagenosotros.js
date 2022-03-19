5717b46e358087b76e3add798651fc2c6a1df115
RobertoD <mitchelldurandb25@gmail.com>
2022-03-19 18:11:47 -0500

    agreg  Nosotros

---
+++ src/Pages/Visitante/Nosotros/Pagenosotros.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Footer from '../../../Components/Footer/Footer'
+import NavbarVisit from '../../../Components/Navbar/NavbarVisitante/NavbarVisit'
+import Nosotros from '../../../Components/Nosotros/Nosotros'
+
+const Pagenosotros = () => {
+  return (
+    <>
+        <NavbarVisit />
+        <Nosotros />
+        <Footer />
+    </>
+  )
+}
+
+export default Pagenosotros
