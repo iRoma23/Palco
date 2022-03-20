8f0c955174881b29d6abedb6285cd98553c44bfc
RobertoD <mitchelldurandb25@gmail.com>
2022-03-20 05:53:52 -0500

    añadiendo logo y mejorando Footer

--- src/AppPrueba.js
+++ src/AppPrueba.js
@@ -3,6 +3,7 @@ import "./App.css";
 
 import Footer from "./Components/Footer/Footer";
 import Navbar from "./Components/Navbar/Navbar";
+import Nosotros from "./Components/Nosotros/Nosotros";
 import NotFoundPages from "./Pages/not_found_404/NotFoundPages";
 import Evento from "./Pages/Propietario/Home/Evento/Evento";
 import PropietarioHome from "./Pages/Propietario/Home/PropietarioHome";
@@ -30,6 +31,7 @@ function AppPrueba() {
         <Route path="/registro_foto" element={<RegistroFotoPage />} />
         <Route path="*" element={<NotFoundPages />} />
       </Routes>
+      <Nosotros />
       <Footer />
     </>
   );
--- src/Components/Nosotros/Nosotros.js
+++ src/Components/Nosotros/Nosotros.js
@@ -8,7 +8,7 @@ const Nosotros = () => {
         <div className="col-md-6">
         <img src={imgpalco} className="img-fluid" alt="..."/>
         </div>
-        <div className="col-md-6 h5 text-center">Somos una empresa dedicada al alquiler de Palcos, trabajamos como intermediarios entre el propietario del Palco y la persona interesada en alquilar alguno brindando la mayor confiabilidad y seguridad a la hora de alquilar un palco para el evento que deseas.
+        <div className="col-md-6 h3 text-center">Somos una empresa dedicada al alquiler de Palcos, trabajamos como intermediarios entre el propietario del Palco y la persona interesada en alquilar alguno brindando la mayor confiabilidad y seguridad a la hora de alquilar un palco para el evento que deseas.
         </div>
       </div>
     </div>
