769802eb028f0a3a98377be9bb05e1c4b6a78152
RobertoD <mitchelldurandb25@gmail.com>
2022-03-20 12:27:16 -0500

    mejorando el Nosotros

--- src/Components/Nosotros/Nosotros.js
+++ src/Components/Nosotros/Nosotros.js
@@ -1,14 +1,24 @@
 import React from "react";
-import imgpalco from '../../Assets/images/palco_nosotros.jpg'
+import imgpalco from "../../Assets/images/palco_nosotros.jpg";
+import logo from "../../Assets/images/logo3.PNG";
 
 const Nosotros = () => {
   return (
-    <div className="container">
+    <div className="container p-3">
+      <div className="">
+        <img className="img-fluid" src={logo} alt="TuPalco" />
+      </div>
       <div className="row justify-content-center">
         <div className="col-md-6">
-        <img src={imgpalco} className="img-fluid" alt="..."/>
+          <img src={imgpalco} className="img-fluid" alt="..." />
         </div>
-        <div className="col-md-6 h3 text-center">Somos una empresa dedicada al alquiler de Palcos, trabajamos como intermediarios entre el propietario del Palco y la persona interesada en alquilar alguno brindando la mayor confiabilidad y seguridad a la hora de alquilar un palco para el evento que deseas.
+        <div className="col-md-6">
+          <p className="text-justify fs-3">
+            Somos una empresa dedicada al alquiler de Palcos, trabajamos como
+            intermediarios entre el propietario del Palco y la persona
+            interesada en alquilar alguno brindando la mayor confiabilidad y
+            seguridad a la hora de alquilar un palco para el evento que deseas.
+          </p>
         </div>
       </div>
     </div>
