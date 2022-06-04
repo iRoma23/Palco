e253c90a58ee54e02abc414b1a48f33e6490a6e6
RobertoD <mitchelldurandb25@gmail.com>
2022-06-04 01:49:31 -0500

    se arreglo el post del palco

--- src/Pages/Propietario/RegistroPalco/RegistroPalcoPage.js
+++ src/Pages/Propietario/RegistroPalco/RegistroPalcoPage.js
@@ -1,130 +1,214 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import palco from './img/palco.jpg'
+import React from "react";
+import { Link } from "react-router-dom";
+import palco from "./img/palco.jpg";
 
 const RegistroPalcoPage = () => {
+  const [PrivateBoxList, setPrivateBoxList] = React.useState([]);
+  const [newBoxNumber, setNewBoxNumber] = React.useState([]);
+  const [newZona, setNewZona] = React.useState([]);
+  const [newDetail, setNewDetail] = React.useState([]);
+
+  function addPrivatebox(e){
+    e.preventDefault()
+    fetch("https://palco-app.herokuapp.com/api/privatebox", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        boxnumber: newBoxNumber,
+        zone: "newZona",
+        detail: newDetail,
+      }),
+    })
+    .then((response) => {
+      console.log("registro exitoso")
+      return response.json();
+      
+    })
+    .then((data) => {
+      getPrivateBox();
+      console.log("mostrando nueva data")
+      });
+  }
+  function handleNewBoxNumberChange(event) {
+    setNewBoxNumber(event.target.value);
+  }
+  function handleNewZonaChange(event) {
+    setNewZona(event.target.value);
+  }
+  function handleNewDetailChange(event) {
+    setNewDetail(event.target.value);
+  }
+  // function handleNewZona2Change(event) {
+  //   setNewZona(event.target.value);
+  // }
+  // function handleNewZona3Change(event) {
+  //   setNewZona(event.target.value);
+  // }
+  // function handleNewZona4Change(event) {
+  //   setNewZona(event.target.value);
+  // }
+
+  function getPrivateBox() {
+    fetch("https://palco-app.herokuapp.com/api/privatebox")
+      .then((response) => response.json())
+      .then((data) => setPrivateBoxList(data));
+  }
+  React.useEffect(() => {
+    getPrivateBox();
+  }, []);
+
   return (
     <>
-      {/* <NavbarVisit /> */}
-      <div className="card-body">
-        <h3 className="text-muted p-1 text-center">
-          INGRESA LOS DATOS Y UBICACIÓN DE TU PALCO
-        </h3>
-      </div>
-      <div className="card">
-        <div className="card-body row g-0">
-          <div className=" card-body col-md-6 ">
-            <div className="text-center">
-              <h3 className="card-title">ESTADIO NACIONAL</h3>
-            </div>
-            <img
-              src={palco}
-              className="img-fluid rounded img-thumbnail"
-             
-              alt="Imagen Estadio"
-            />
-          </div>
-
-          {/* aca va el formlario */}
-          <div className=" col-md-6">
-            <div className="card-body">
-              <h3 className="text-center">REGISTRA TU PALCO</h3>
-            </div>
-            <div className="card p-3">
-              <div className="">
-                <select
-                  class="form-select form-select-lg mb-3"
-                  aria-label=".form-select-lg example"
-                >
-                  <option selected>Seleccione Estadio</option>
-                  <option value="1">Estadio Nacional</option>
-                  <option value="2">Estadio Monumental</option>
-                  <option value="3">Monumental UNSA</option>
-                  <option value="4">Alejandro Villanueva</option>
-                  <option value="5">Inca Garcilazo de la Vega</option>
-                  <option value="6">Estadio Mansiche</option>
-                </select>
-                <input
-                  type="text"
-                  class="form-control"
-                  id="exampleFormControlInput1"
-                  placeholder="Numero de Palco"
-                />
-                <input
-                  type="text"
-                  class="form-control"
-                  id="exampleFormControlInput1"
-                  placeholder="Detalle"
-                />
+      <form onSubmit={(e)=>addPrivatebox(e)}>
+        <div className="card-body">
+          <h3 className="text-muted p-1 text-center">
+            INGRESA LOS DATOS Y UBICACIÓN DE TU PALCO
+          </h3>
+        </div>
+        <div className="card">
+          <div className="card-body row g-0">
+            <div className=" card-body col-md-6 ">
+              <div className="text-center">
+                <h3 className="card-title">ESTADIO NACIONAL</h3>
               </div>
-              {/* Aca va los checkbox */}
-              <div className="card-body">
-                <div class="form-check ">
-                  <input
-                    class="form-check-input"
-                    type="checkbox"
-                    value=""
-                    id="flexCheckDefault"
-                  />
-                  <label class="form-check-label" for="flexCheckDefault">
-                    Zona Norte
-                  </label>
-                </div>
-                <div class="form-check">
-                  <input
-                    class="form-check-input"
-                    type="checkbox"
-                    value=""
-                    id="flexCheckChecked"
-                  />
-                  <label class="form-check-label" for="flexCheckChecked">
-                    Zona Oriente
-                  </label>
-                </div>
-                <div class="form-check">
-                  <input
-                    class="form-check-input"
-                    type="checkbox"
-                    value=""
-                    id="flexCheckChecked"
-                  />
-                  <label class="form-check-label" for="flexCheckChecked">
-                    Zona Poniente
-                  </label>
-                </div>
-                <div class="form-check">
-                  <input
-                    class="form-check-input"
-                    type="checkbox"
-                    value=""
-                    id="flexCheckChecked"
-                  />
-                  <label class="form-check-label" for="flexCheckChecked">
-                    Zona Sur
-                  </label>
-                </div>
-                {/* Aca va el Boton */}
-                <div className=" gap-2 d-md-flex justify-content-md-end mt-2">
-                  <Link type="button" class="btn btn-primary" to="/registro_propietario">
-                    Atras
-                  </Link>
-                  <Link type="button" class="btn btn-primary " to="/registro_exitoso">
-                    Registrar
-                  </Link>
+              <img
+                src={palco}
+                className="img-fluid rounded img-thumbnail"
+                alt="Imagen Estadio"
+              />
+            </div>
 
+            {/* aca va el formlario */}
+            <div className=" col-md-6">
+              <div className="card-body">
+                <h3 className="text-center">REGISTRA TU PALCO</h3>
+              </div>
+              <div className="card p-3">
+                <div className="">
+                  <select
+                    class="form-select form-select-lg mb-3"
+                    aria-label=".form-select-lg example"
+                  >
+                    <option selected>Seleccione Estadio</option>
+                    <option value="1">Estadio Nacional</option>
+                    <option value="2">Estadio Monumental</option>
+                    <option value="3">Monumental UNSA</option>
+                    <option value="4">Alejandro Villanueva</option>
+                    <option value="5">Inca Garcilazo de la Vega</option>
+                    <option value="6">Estadio Mansiche</option>
+                  </select>
+                  {/* Aca va los checkbox */}
+                  <div>
+                    <fieldset>
+                      <legend>Selecciona la ubicación de tu Palco:</legend>
+                      <div>
+                        <input
+                          type="radio"
+                          id="norte"
+                          name="zona"
+                          // value={newZona}
+                          // onChange={handleNewZonaChange}
+                        />
+                        <label for="norte">Zona Norte</label>
+                      </div>
+                      <div>
+                        <input
+                          type="radio"
+                          id="oriente"
+                          name="zona"
+                          value={newZona}
+                          onChange={handleNewZonaChange}
+                        />
+                        <label for="oriente">Zona Oriente</label>
+                      </div>
+                      <div>
+                        <input
+                          type="radio"
+                          id="poniente"
+                          name="zona"
+                          // value={newZona}
+                          // onChange={handleNewZona3Change}
+                        />
+                        <label for="poniente">Zona Poniente</label>
+                      </div>
+                      <div>
+                        <input 
+                          type="radio" 
+                          id="sur" 
+                          name="zona" 
+                          // value={newZona}
+                          // onChange={handleNewZona4Change} 
+                        />
+                        <label for="sur">Zona Sur</label>
+                      </div>
+                    </fieldset>
+                  </div>
+                  <br/>
+                  <input
+                    type="text"
+                    class="form-control"
+                    value={newBoxNumber}
+                    onChange={handleNewBoxNumberChange}
+                    id="exampleFormControlInput1"
+                    placeholder="Numero de Palco"
+                  />
+                  <br />
+                  <input
+                    type="text"
+                    class="form-control"
+                    value={newDetail}
+                    onChange={handleNewDetailChange}
+                    id="exampleFormControlInput1"
+                    placeholder="Detalle"
+                  />
                 </div>
-                <p className="text-center ">
-                  <small className="text-muted">Last updated 3 mins ago</small>
-                </p>
+
+                <div className="card-body">
+                  {/* Aca va el Boton */}
+                  <div className=" gap-2 d-md-flex justify-content-md-end mt-2">
+                    <Link
+                      type="button"
+                      class="btn btn-primary"
+                      to="/registro_propietario"
+                    >
+                      Atras
+                    </Link>
+                    <Link
+                      type="button"
+                      class="btn btn-primary "
+                      to="/registro_exitoso"
+                      // onClick={addPrivatebox}
+                    >
+                      Registrar
+                    </Link>
+                  </div>
+                  <button type="submit">Registrar2</button>
+                  
+                  {/* <p className="text-center ">
+                    <small className="text-muted">
+                      Last updated 3 mins ago
+                    </small>
+                  </p> */}
+                </div>
               </div>
             </div>
           </div>
         </div>
-
-
+      </form>
+       <div className="text-center">
+        <h2>LISTA DE PALCOS:</h2>
+        <ul>
+          {PrivateBoxList.map((box) => {
+            return (
+              <li>
+                {box._id}, {box.zone}, {box.detail}
+              </li>
+            );
+          })}
+        </ul>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default RegistroPalcoPage
+export default RegistroPalcoPage;
--- src/Pages/Propietario/RegistroPalco/RegistroPropietarioPage.js
+++ src/Pages/Propietario/RegistroPalco/RegistroPropietarioPage.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import "./style.css";
 
 const RegistroPropietarioPage = () => {
-  const [OwnerList, setOwnerList] = React.useState([]);
+  // const [OwnerList, setOwnerList] = React.useState([]);
   const [newEmail, setNewEmail] = React.useState([]);
   const [newName, setNewName] = React.useState([]);
   const [newLast_name, setNewLast_name] = React.useState([]);
@@ -12,21 +12,26 @@ const RegistroPropietarioPage = () => {
 
   function addOwner() {
     fetch("https://palco-app.herokuapp.com/api/owner", {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ 
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
         email: newEmail,
         name: newName,
         last_name: newLast_name,
         dni: newDni,
-        phone: newPhone
-       })
-       }).then((response)=>{
-        return response.json()
-      }).then((data) =>{
-        getOwner()
-    })
+        phone: newPhone,
+      }),
+    }).then((response) => {
+      console.log("registro exitoso");
+      console.log(response.json());
+      return response.json();
+    });
+    // .then((data) => {
+    //   getOwner();
+    //   console.log("mostrando nueva data")
+    // });
   }
+
   function handleNewEmailChange(event) {
     setNewEmail(event.target.value);
   }
@@ -42,117 +47,109 @@ const RegistroPropietarioPage = () => {
   function handleNewPhoneChange(event) {
     setNewPhone(event.target.value);
   }
-  function getOwner() {
-    fetch("https://palco-app.herokuapp.com/api/owner")
-      .then((response) => response.json())
-      .then((data) => setOwnerList(data));
-  }
-  React.useEffect(() => {
-    getOwner();
-  }, []);
+  // function getOwner() {
+  //   fetch("https://palco-app.herokuapp.com/api/owner")
+  //     .then((response) => response.json())
+  //     .then((data) => setOwnerList(data));
+  // }
+  // React.useEffect(() => {
+  //   getOwner();
+  // }, []);
 
   return (
     <>
-      <form action="#" className="formulario ">
-        <div className="container ">
-          <div className="col-sm-6">
-            <div className="text-center pb-2">
-              <h3>Registrar Propietario</h3>
-            </div>
-            <div className="mb-3">
+      <form action="" className="formulario" id="formul">
+        <fieldset>
+          <div className="container ">
+            <div className="col-sm-6">
+              <div className="text-center pb-2">
+                <h3>Ingresar Datos del Prop</h3>
+              </div>
               <div className="mb-3">
-                <input
-                  id="email"
-                  value={newEmail}
-                  onChange={handleNewEmailChange}
-                  type="email"
-                  class="form-control"
-                  aria-describedby="emailHelp"
-                  placeholder="Correo Electronico"
-                  // value = {email}
-                />
-              </div>
-              <div class="mb-3">
-                <input
-                  id="nombres"
-                  value={newName}
-                  onChange={handleNewNameChange}
-                  type="text"
-                  class="form-control"
-                  placeholder="Nombres"
-                  // value = {password}
-                />
-              </div>
+                <div className="mb-3">
+                  <input
+                    id="email"
+                    value={newEmail}
+                    onChange={handleNewEmailChange}
+                    type="email"
+                    class="form-control"
+                    aria-describedby="emailHelp"
+                    placeholder="Correo Electronico"
+                  />
+                </div>
+                <div class="mb-3">
+                  <input
+                    id="nombres"
+                    value={newName}
+                    onChange={handleNewNameChange}
+                    type="text"
+                    class="form-control"
+                    placeholder="Nombres"
+                  />
+                </div>
 
-              <div class="mb-3">
-                <input
-                  id="apellidos"
-                  value={newLast_name}
-                  onChange={handleNewLast_nameChange}
-                  type="text"
-                  class="form-control"
-                  placeholder="Apellidos"
-                  // value = {password}
-                />
-              </div>
+                <div class="mb-3">
+                  <input
+                    id="apellidos"
+                    value={newLast_name}
+                    onChange={handleNewLast_nameChange}
+                    type="text"
+                    class="form-control"
+                    placeholder="Apellidos"
+                  />
+                </div>
 
-              <div class="mb-3">
-                <input
-                  id="dni"
-                  value={newDni}
-                  onChange={handleNewDniChange}
-                  type="text"
-                  class="form-control"
-                  placeholder="Dni"
-                  // value = {password}
-                />
-              </div>
-              <div class="mb-3">
-                <input
-                  id="telefono"
-                  value={newPhone}
-                  onChange={handleNewPhoneChange}
-                  type="number"
-                  class="form-control"
-                  placeholder="Telefono"
-                  // value = {password}
-                />
-              </div>
+                <div class="mb-3">
+                  <input
+                    id="dni"
+                    value={newDni}
+                    onChange={handleNewDniChange}
+                    type="text"
+                    class="form-control"
+                    placeholder="Dni"
+                  />
+                </div>
+                <div class="mb-3">
+                  <input
+                    id="telefono"
+                    value={newPhone}
+                    onChange={handleNewPhoneChange}
+                    type="number"
+                    class="form-control"
+                    placeholder="Telefono"
+                  />
+                </div>
 
-              <div class="mb-3 form-check">
-                <input
-                  className="form-check-input"
-                  type="checkbox"
-                  id="gridCheck"
-                />
-                <label
-                  className="form-check-label color-text bold"
-                  for="gridCheck"
-                >
-                  Acepto los Términos y condiciones
-                </label>
+                <div class="mb-3 form-check">
+                  <input
+                    className="form-check-input"
+                    type="checkbox"
+                    id="gridCheck"
+                  />
+                  <label
+                    className="form-check-label color-text bold"
+                    for="gridCheck"
+                  >
+                    Acepto los Términos y condiciones
+                  </label>
+                </div>
+                <div className="text-center">
+                  <Link
+                    to="/registro_palco"
+                    onClick={addOwner}
+                    type="submit"
+                    className="btn btn-primary btn-lg"
+                  >
+                    Continuar
+                  </Link>
+                </div>
+                <button onClick={addOwner}>Boton Enviar</button>
               </div>
-
-              <div className="text-center">
-                <Link
-                  to="/registro_palco"
-                  type="submit"
-                  className="btn btn-primary btn-lg"
-                >
-                  Registrar
-                </Link>
-              </div>
-              <button
-                onClick={addOwner}
-                className="text-center, btn btn-primary btn-lg"
-              >
-                Registrarnew
-              </button>
             </div>
           </div>
-        </div>
+        </fieldset>
       </form>
-      <div className="text-center">
+      {/* <div className="text-center">
         <h2>LISTA DE PROPIETARIOS:</h2>
         <ul>
           {OwnerList.map((owner) => {
@@ -163,7 +160,7 @@ const RegistroPropietarioPage = () => {
             );
           })}
         </ul>
-      </div>
+      </div> */}
     </>
   );
 };
